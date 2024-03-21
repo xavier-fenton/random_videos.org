bfbb5098895c4c97276be4a4a6c7771e3fc8128c
Xavier Fenton <xavierfenton99@gmail.com>
2024-03-21 13:53:55 +1300

    create new loader for video player

--- src/app/videosColumn.tsx
+++ src/app/videosColumn.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export const VideoColumn: React.FC<Props> = ({ usersVideoCollection }) => {
   
-  const {setVideoPlayer, setLoader} = useVideoContext()
+  const {setVideoPlayer, loader2, setLoader2} = useVideoContext()
 
   
   
@@ -21,11 +21,11 @@ export const VideoColumn: React.FC<Props> = ({ usersVideoCollection }) => {
         className="flex border-2 border-[#d8cbcb] w-full h-full bg-[#d8cbcb] text-center justify-center items-center"
       >
         <button onClick={() =>{
-          setLoader(true)
+          setLoader2(true)
           setTimeout(() => {
-            setVideoPlayer(video.video_file)
-            setLoader(false)
-          }, 2000)
+            setVideoPlayer(video)
+            setLoader2(false)
+          }, 1500)
         }}>
           <video key={video.testkey} className="h-full" >
             <source src={video.video_file} type="video/mp4" />
