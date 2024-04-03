d3093da35be1eb72ebb637fe7c71104b61fa1763
Xavier Fenton <xavierfenton99@gmail.com>
2024-04-04 09:59:36 +1300

    clean up file

--- pages/profile/[username].tsx
+++ pages/profile/[username].tsx
@@ -1,34 +1,21 @@
 // pages/[username].js
 import '../../src/app/globals.css'
-import { VideoPlayerProvider } from '@component/app/_context/videoContext';
-import { UserProfile } from '../../src/app/_components/userprofile';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import { useFireBaseAuth } from '@component/app/_providers/FireBaseAuthProvider';
-// import { TestUsers as user } from '@component/app/testUsers';
+import { VideoPlayerProvider } from '@component/app/_context/videoContext'
+import { UserProfile } from '../../src/app/_components/userprofile'
+import AuthLayout from '../../layouts/AuthLayout'
 
 const UserPage = () => {
-  const { user } = useFireBaseAuth()
-   const router = useRouter();
-  const { username } = router.query;
-
-  useEffect(() => {
-    // You can fetch user data or perform other actions based on the username
-    console.log('Hello:', username);
-  }, [username]);
-
   return (
-    <VideoPlayerProvider>
-      {/* {user && user.map((user, index) => {
-        if(username === user.username){
+    <>
+      <AuthLayout>
+          <VideoPlayerProvider>
+           
+            <UserProfile />
+          </VideoPlayerProvider>
+        
+      </AuthLayout>
+    </>
+  )
+}
 
-          return <UserProfile key={index} user={user}/>
-        } 
-      })} */}
-      <UserProfile key={1} user={user}/>
-
-    </VideoPlayerProvider>
-  );
-};
-
-export default UserPage;
+export default UserPage
